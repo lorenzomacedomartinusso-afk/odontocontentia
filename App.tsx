9255215ec933cf13f2c78edd6b4b204980cb08e4
lorenzomacedomartinusso-afk <lorenzomacedomartinusso@gmail.com>
2026-01-01 22:24:31 -0300

    style: Increase maximum width of main content area for better utilization.

--- App.tsx
+++ App.tsx
@@ -1752,7 +1752,7 @@ const Wizard: React.FC<{
   }
 
   return (
-    <div className="flex flex-col h-full w-full max-w-[95%] xl:max-w-[90%] 2xl:max-w-[85%] mx-auto">
+    <div className="flex flex-col h-full w-full max-w-[98%] xl:max-w-[95%] 2xl:max-w-[92%] mx-auto">
       {/* Trial Counter Banner */}
       {/* Trial Counter Banner */}
       {/* Trial Counter Banner */}
